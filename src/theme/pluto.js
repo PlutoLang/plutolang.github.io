562ded934f2e7703bab35256d1dc21ee146c1ba1
Sainan <sainan@calamity.gg>
2023-03-22 14:12:06 +0100

    Syntax highlighting: Classify true & false as 'boolean' instead of 'keyword'

--- src/theme/pluto.js
+++ src/theme/pluto.js
@@ -4,8 +4,9 @@ Prism.languages.pluto = {
 		pattern: /(["'])(?:(?!\1)[^\\\r\n]|\\z(?:\r\n|\s)|\\(?:\r\n|[^z]))*\1|\[(=*)\[[\s\S]*?\]\2\]/,
 		greedy: true
 	},
+	'boolean': /\b(?:true|false)\b/,
 	'number': /\b([\d_]+|(0x[a-f\d]+)|(0b[01]+))(?:\.[a-f\d]*)?(?:p[+-]?\d+)?\b|\b\d+(?:\.\B|(?:\.\d*)?(?:e[+-]?\d+)?\b)|\B\.\d+(?:e[+-]?\d+)?\b/i,
-	'keyword': /\b(?:and|as|enum|begin|break|when|do|else|elseif|end|false|for|function|goto|if|in|local|nil|not|or|repeat|return|then|true|until|while|continue|switch|case|default|pluto_case|pluto_switch|pluto_continue|pluto_default)\b/,
+	'keyword': /\b(?:and|as|enum|begin|break|when|do|else|elseif|end|for|function|goto|if|in|local|nil|not|or|repeat|return|then|until|while|continue|switch|case|default|pluto_case|pluto_switch|pluto_continue|pluto_default)\b/,
 	'function': /(?!\d)\w+(?=\s*(?:[({]))/,
 	'operator': [
 		/[-+*%^&|#]|\/\/?|<[<=]?|>[>=]?|[=~]=?/,
