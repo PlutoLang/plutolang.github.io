8f82105eaf118784f3c0e7cf5ad57ce6ca6eb6d2
yeetNspagoot <yeetnspagoot@proton.me>
2026-08-08 13:19:36 +1000

    Update Lua version to 5.5 in metadata

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,11 @@ export default function Home() {
     <>
       <Head>
         <title>Pluto</title>
-        <meta name="description" content="A superset of Lua 5.4 with a focus on general-purpose programming." />
+        <meta name="description" content="A superset of Lua 5.5 with a focus on general-purpose programming." />
         <meta property="og:title" content="Pluto" />
-        <meta property="og:description" content="A superset of Lua 5.4 with a focus on general-purpose programming." />
+        <meta property="og:description" content="A superset of Lua 5.5 with a focus on general-purpose programming." />
         <meta name="twitter:title" content="Pluto" />
-        <meta name="twitter:description" content="A superset of Lua 5.4 with a focus on general-purpose programming." />
+        <meta name="twitter:description" content="A superset of Lua 5.5 with a focus on general-purpose programming." />
       </Head>
       <Redirect to="/docs/Introduction" />
     </>
